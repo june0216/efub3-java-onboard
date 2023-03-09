2f9238a73da073af1781318f977470accd0f5901
June <ziyun1612@naver.com>
2023-03-09 17:19:46 +0900

    :sparkles: feat : solve Problem1

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -6,6 +6,17 @@ import java.util.List;
 public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
         List<Integer> answer = new ArrayList<>();
+        if(minute < 45) {
+            hour--;
+            minute= 60 - (45 - minute);
+            if(hour < 0) {
+                hour = 23;
+            }
+            answer = List.of(hour, minute);
+        }
+        else {
+            answer = List.of(hour, minute- 45);
+        }
         return answer;
     }
 }
